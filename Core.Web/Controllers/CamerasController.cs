cdea0d6b558032217c8b6467652de4535b58f3f8
KaianHenriqueVargas <tec.kaian@hotmail.com>
2026-01-31 22:10:07 -0300

    Melhorias

--- Core.Web/Controllers/CamerasController.cs
+++ Core.Web/Controllers/CamerasController.cs
@@ -1,11 +1,14 @@
 using Core.Web.Aplicacao.Cameras.Comandos;
 using Core.Web.Aplicacao.Cameras.Dtos;
+using Core.Web.Enumerados;
 using Lib.Infraestrutura.Controllers;
 using Lib.Infraestrutura.MediatR.Abstracoes;
 using Lib.Infraestrutura.Validacoes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core.Web.Controllers;
 
+[ApiExplorerSettings(GroupName = nameof(GruposDeEndpoints.Cameras))]
 public class EscanearCamerasNaRedeController(
     IMediatorHandler bus,
     IValidadorDominio validadorDominio,
